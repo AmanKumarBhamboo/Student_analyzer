d85d95e0c0c8165c75ec84585aed4ab12bb0459b
Drazo <amankumarbhamboo14022005@gmail.com>
2026-04-12 06:02:12 +0530

    Updated logger and exception

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,18 @@ import logging
 import os
 from datetime import datetime
 
-# Log file ka naam current timestamp ke saath
+# 1. File ka naam decide karein
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# Make logs folder if not there and if there just append it.
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+# 2. Folder ka path (Sirf 'logs' folder tak)
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# 3. Final File Path (logs folder ke andar file)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(
-    filename=logs_path,
+    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
