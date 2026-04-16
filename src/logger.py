bf099ed2b67615ccc75bca3cdef80041c63fbb09
Drazo <amankumarbhamboo14022005@gmail.com>
2026-04-16 20:52:41 +0530

    Chota sa push

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# 1. File ka naam decide karein
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# 2. Folder ka path (Sirf 'logs' folder tak)
-logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)
-
-# 3. Final File Path (logs folder ke andar file)
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
